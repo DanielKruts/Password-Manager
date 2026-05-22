f47d19d1dfd221f8c288d81f29d7f2784623ba10
DanielKruts <djkrutsick@icloud.com>
2026-05-22 15:28:56 -0400

    Edited and changed the README to be catered towards tracking my progress even further than the commit history. Also created a small and very basic hashing utility file that will be helpful in the future when making the application for the password manager. Will probably end up larger as I add more functionality to the file.

---
+++ HashingUtils.py
@@ -0,0 +1,35 @@
+import hashlib # Used for all of the fun scrypt functions for hashing passwords, verifying passwords, etc.
+import os # Used for the cryptographically secure RNG, os.urandom
+
+#-----------------------------------------------------------------------#
+# Parameters                                                            #
+# N(n): Computational cost(CPU)/Memory cost factor. Must be power of 2  #
+# r: Block size (Typically 8)                                           #
+# p: Parallelization factor(Typically 1)                                #
+# klen: Desired Key Length (e.g. 32 for a 256-bit key)                  #
+#-----------------------------------------------------------------------#
+N_COST = 2 ** 14 # 2 ^ 14
+BLOCK_SIZE = 8
+P = 1
+klen = 32
+
+# Takes a desired password and converts it into a byte formatting for hashlib.scrypt to properly handle
+# hashPW returns a byte formatted string of salt + the 
+def hashPW(password: str) -> bytes:
+    s = os.urandom(16) # 16 bytes of salt\
+    key = hashlib.scrypt(password.encode('utf-8'), salt=s, n=N_COST, r=BLOCK_SIZE, p=P, dklen=klen)
+    return s + key
+
+# Derives the key portion from the attempted password and then compares it to the key stored inside of the database
+# Returns true if the password was the same and false if they are not the same, pretty straight forward
+def verifyPW(storedHash: bytes, attemptedPW: str) -> bool:
+    s = storedHash[:16]#First 16 bytes is the salt
+    key = storedHash[16:]#Last 16 bytes is the key
+
+    try:
+        derivedKey = hashlib.scrypt(attemptedPW.encode('utf-8'), salt=s, n=N_COST, r=BLOCK_SIZE, p=P, dklen=klen)
+        return derivedKey == key
+    except ValueError:
+        print("Something went wrong.")
+        return False
+    
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Password-Manager
-A password manager that I'm building from scratch. I'm utilizing Python and the bcrypt libraries, as well as SQLite to keep track of the passwords in a database
+A password manager that I'm building from scratch. I'm utilizing Python and the hashlib libraries, specifcally scrypt, as well as SQLite to keep track of the passwords in a database
+
+## Task list of things to do
+    - [x] Start writing the HashingUtils.py file
+    - [x] Do research on password managers and how they work
+    - [ ] Figure out and understand exactly when and where the passwords should be hashed or encrypted, and how the client end works in a password manager
+    - [ ] Create a database and play around with one to remind myself of my SQL skills and how to properly setup the database to my needs
+    - [ ] Research about how to make python scripts to interact with the database directly like querying and entering data
+    - [ ] Start a document that shows the structure of the project to keep myself logically sound and accurate in my implementation
+    - [ ] Build the small application with visuals that allows me to login with a username and password to access my section of the database
+
+
+## 5/22/2026
+    - Created a general basis for the project by starting to understand the hashlib library and playing around with some of the functions
+    - Edited the README and started to create a general task list to keep myself on track in this README
+    - 
